0f0a8341fcf417d2c1a9e9466be100229cc8fdbe
VMakhakhei <silenthealsqwe@gmail.com>
2023-03-29 18:46:18 +0200

    Lesson 10 first commit (finish refactor code)

--- demo_qa_lesson5/registration_form.py
+++ demo_qa_lesson5/registration_form.py
@@ -2,6 +2,24 @@ from selene import browser, be, have, by
 import os
 
 class RegistrationPage:
+    def __init__(self):
+        self.first_name = browser.element("#firstName")
+        self.last_name = browser.element("#lastName")
+        self.email = browser.element("#userEmail")
+        self.gender_male = browser.element('[for=gender-radio-1]')
+        self.gender_woman = browser.element('[for=gender-radio-2]')
+        self.gender_other = browser.element('[for=gender-radio-3]')
+        self.user_number = browser.element("#userNumber")
+        self.current_adress = browser.element('#currentAddress')
+        self.state = browser.element('#react-select-3-input')
+        self.city = browser.element('#react-select-4-input')
+        self.subjects = browser.element('#subjectsInput')
+        self.hobby_sports = browser.element('#hobbiesWrapper label[for="hobbies-checkbox-1"]')
+        self.hobby_reading = browser.element('#hobbiesWrapper label[for="hobbies-checkbox-2"]')
+        self.hobby_music = browser.element('#hobbiesWrapper label[for="hobbies-checkbox-3"]')
+        self.upload_picture = browser.element(by.id('uploadPicture'))
+
+
     def open(self):
         browser.open('/automation-practice-form')
 
@@ -27,42 +45,42 @@ class RegistrationPage:
 
     def gender(self, gender):
         if gender.lower() == 'male':
-            browser.element('[for=gender-radio-1]').click()
+            self.gender_male.click()
         elif gender.lower() == 'female':
-            browser.element('[for=gender-radio-2]').click()
+            self.gender_woman.click()
         elif gender.lower() == 'other':
-            browser.element('[for=gender-radio-3]').click()
+            self.gender_other.click()
         else:
             raise AttributeError
 
     def type_phone(self, phone_number):
-        browser.element("#userNumber").should(be.blank).type(phone_number)
+        self.user_number.should(be.blank).type(phone_number)
 
     def click_submit(self):
         browser.execute_script("document.querySelector('#submit').click()")
 
     def fill_current_address(self, current_address):
-        browser.element('#currentAddress').should(be.blank).type(current_address)
+        self.current_adress.should(be.blank).type(current_address)
         pass
 
     def select_state(self, state):
-        browser.element('#react-select-3-input').should(be.blank).type(state).press_enter()
+        self.state.should(be.blank).type(state).press_enter()
 
     def select_city(self, city):
-        browser.element('#react-select-4-input').should(be.blank).type(city).press_enter()
+        self.city.should(be.blank).type(city).press_enter()
 
     def upload_picture(self, picture_path):
-        browser.element(by.id('uploadPicture')).send_keys(os.getcwd() + picture_path)
+        self.upload_picture.send_keys(os.getcwd() + picture_path)
 
     def select_subjects(self, subjects):
-        browser.element('#subjectsInput').should(be.blank).type(subjects).press_enter()
+        self.subjects.should(be.blank).type(subjects).press_enter()
 
     def select_hobbies(self, hobbies):
         if hobbies.lower() == 'music':
-            browser.element('#hobbiesWrapper label[for="hobbies-checkbox-3"]').click()
+            self.hobbie_music.click()
         elif hobbies.lower() == 'reading':
-            browser.element('#hobbiesWrapper label[for="hobbies-checkbox-2"]').click()
+            self.hobbie_reading.click()
         elif hobbies.lower() == 'sports':
-            browser.element('#hobbiesWrapper label[for="hobbies-checkbox-1"]').click()
+            self.hobbie_sports.click()
         else:
             raise AttributeError
