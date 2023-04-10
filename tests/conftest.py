3a56f17dc50dd086d419eea4eb8f4ec5e39a2b4f
VMakhakhei <silenthealsqwe@gmail.com>
2023-04-10 17:53:20 +0200

    Lesson 10

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,3 @@ def browser_setup():
     browser.config.window_height = 1080
     browser.config.hold_browser_open = True
     browser.config.base_url = 'https://demoqa.com'
-
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
