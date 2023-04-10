1bc645f36ac2683c4dd1f76d5ce8c4233d2a3e21
VMakhakhei <silenthealsqwe@gmail.com>
2023-04-10 18:39:17 +0200

    Lesson 10

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 from selene.support.shared import browser
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
@@ -10,6 +13,23 @@ def browser_setup():
     browser.config.window_height = 1080
     browser.config.hold_browser_open = True
     browser.config.base_url = 'https://demoqa.com'
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
 
     yield
 
