bb3c5b9d46316a23680e50cf0f3a229d93695feb
VMakhakhei <silenthealsqwe@gmail.com>
2023-04-10 17:38:39 +0200

    Lesson 10

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selene.support.shared import browser
+from utils import attach
 
 
 @pytest.fixture
@@ -9,3 +10,6 @@ def browser_setup():
     browser.config.hold_browser_open = True
     browser.config.base_url = 'https://demoqa.com'
 
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
