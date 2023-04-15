0310308b43d1e5b883b63e377029d9c5c50853e0
VMakhakhei <silenthealsqwe@gmail.com>
2023-04-15 11:11:32 +0200

    Lesson 10

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,4 @@ from selene.support.shared import browser
 def browser_setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.hold_browser_open = True
     browser.config.base_url = 'https://demoqa.com'
