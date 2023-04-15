a21ce2d683ae9d8b5756ad22298cc542db63d837
VMakhakhei <silenthealsqwe@gmail.com>
2023-04-15 13:13:34 +0200

    Lesson 10

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,27 @@
 import pytest
-from selene.support.shared import browser
+from selene import Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils.attach import add_logs, add_html, add_screenshot
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def browser_setup():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {"enableVNC": True, "enableVideo": False},
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options,
+    )
+    browser = Browser(Config(driver))
+
+    yield browser
+
+    add_html(browser)
+    add_screenshot(browser)
+    add_logs(browser)
